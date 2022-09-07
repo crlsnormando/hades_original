095905db5806db7b15d7e6fd63fc59e947c83a70
Normando <crlsnormando@gmail.com>
2022-09-07 18:11:49 -0300

    criando mensagem de erro com javascript

--- core/erp/view/views.py
+++ core/erp/view/views.py
@@ -21,7 +21,7 @@ class CategoryListView(ListView):
     template_name = 'category/list.html'
     context_object_name = 'category'
 
-# utilizado para recuperar o get e o post
+# utilizado para recuperar o get e o post antes de iniciar o o get e post
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
         if request.method == 'GET':
@@ -56,12 +56,32 @@ class CategoryCreateView(CreateView):
     template_name = 'category/create.html'
     success_url = reverse_lazy('erp:category_list')
 
+    def post(self, request, *args, **kwargs):
+        data={}
+        try:
+            action = request.POST['action']
+            if action == 'add':
+                #form = CategoryForm(request.POST) uma forma de obter o formulario
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'Não há nenhum opção'
+            #data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
 
+
+    # def post(self, request, *args, **kwargs):
+    #     print(request.POST)
+    #     form = CategoryForm(request.POST)
+    #     print(dir(form))
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
         context['title'] = 'Formulario Categorias'
         context['list_url'] = reverse_lazy('erp:category_list')
         context['entity'] = "Categorias"
+        context['action'] = "add"
 
         return context
